cb96a31034e9f1aedcc6bdf5bab4ebc98a16fe98
Abagail Hudson <abagailanne@gmail.com>
2019-05-14 00:11:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => 
+		:railroads =>
 		{:pieces => 4, :names => {} ,
 			:rent_in_dollars =>{}
 		}
@@ -25,15 +25,15 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-		:railroads => 
+		:railroads =>
 		{
-			:pieces => 4, :names => 
+			:pieces => 4, :names =>
 			{
 			:reading_railroad =>{},
 			:pennsylvania_railroad =>{},
 			:b_and_o_railroad =>{},
 			:shortline =>{}
-			} , 
+			} ,
 			:rent_in_dollars => {
 				:one_piece_owned => 25 ,
 				:two_pieces_owned => 50,
